e708ec3288c332a0448296260cdc0375341db79f
CESCO <cescovalledev@gmail.com>
2024-08-29 16:51:59 -0600

    routing and detail page added

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import {HousingLocation} from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink ],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
